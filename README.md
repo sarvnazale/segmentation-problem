63edb818868f180eb406cf69bb61b33228a72f04
sarvnazale <55462526+sarvnazale@users.noreply.github.com>
2022-07-23 13:19:28 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# segmentation-problem
- aUToronto Vision Baseline Submission
+# Segmentation Submission for aUToronto
+This is my attempt at solving the aUToronto Perception Detection - 2D Object Detection Team's coding challenge. I did not have a lot of experience with image segmentation but wanted to see how much I could learn in two-three days. 
+
+## How my code works
+This program uses k-means clustering algorithm to partition the input image into clusters. An attempt is made to optimize the cluster number by using the [Elbow Method](https://www.analyticsvidhya.com/blog/2021/01/in-depth-intuition-of-k-means-clustering-algorithm-in-machine-learning/#:~:text=Elbow%20Method,-In%20the%20Elbow&text=WCSS%20is%20the%20sum%20of,is%20largest%20when%20K%20%3D%201.). To detect the region where the rate of change of inertia starts to decrease, a for loop compares the change in intertia between three consecutive points to detect the "elbow". Based on this the number of clusters is detected and used to perform k-means clustering. 
+
+## How to run my code
+Required Libraries: numpy, matplotlib, cv2, scikit-learn
+Additional Libraries: tkinter, os
+Run segment.py and choose an input image when prompted to do so. The output image is saved under the output folder. 
+
+## Issues/Next Steps 
+1. Optimizing cluster count 
+The current method of finding the "elbow" of the curve for cluster count needs improvement. 
+
+2. Choosing clusters to mask 
+The current code assumes that the cluster code does 
---
+++ segment.py
@@ -0,0 +1,84 @@
+##import necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+from sklearn.cluster import KMeans
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename 
+import os
+
+
+def optimal_cluster(dataset):
+    inertia = []
+    K = range(1,10) #maximum of 10 clusters being considered 
+    
+    ##use sum of squared distance/elbow method to find inertia vs. k value 
+    for num_clusters in K :
+        kmeans = KMeans(n_clusters=num_clusters) 
+        kmeans.fit(dataset)
+        inertia.append(kmeans.inertia_) 
+
+    ##attempt to intelligently discern k value from the elbow method
+    for i in range(6): 
+        k_value = 4 #default k value if condition fails 
+        first_difference = inertia[i + 1] - inertia[i + 2]
+        second_difference =  inertia[i + 2] - inertia[i +3]
+        #if the difference between the three consecutive values is drastic, then pick the third value (past the "elbow" of the curve)
+        if second_difference < 0.25 * first_difference: 
+            k_value = i + 3 
+            print("Chosen K value", k_value) #print chosen value to compare with elbow
+            break
+
+    ##uncomment this section to view the results of the elbow method and compare it with the chosen k value
+    #plt.plot(K, inertia,'bx-') 
+    #plt.title('Elbow Method')
+    #plt.xlabel('Values of K') 
+    #plt.ylabel('Inertia') 
+    #plt.show()
+    
+    return k_value
+
+def masking(k_value, labels, dataset, image, image_path): 
+    masked_image = np.copy(dataset) 
+    
+    masked_image = masked_image.reshape((-1, 3)) # convert to the shape of pixel values
+    
+    #set all clusters but one to black
+    for i in range(k_value): 
+        if i == 3: 
+            masked_image[labels == i] = [255, 255, 255] #set cluster to white
+        else: 
+            masked_image[labels == i] = [0, 0, 0]
+
+    masked_image = masked_image.reshape(image.shape) #reshape to original shape
+    plt.imshow(masked_image) #display new image
+    output_path = "output/" + os.path.basename(image_path)
+    print(output_path)
+    cv2.imwrite(output_path, masked_image)
+
+def main():
+    Tk().withdraw()
+    image_path = askopenfilename()
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    pixel_values = image.reshape((-1, 3))
+
+    pixel_values = np.float32(pixel_values) # convert to float as required by cv2.kmeans
+    # setting stopping criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+    k = optimal_cluster(pixel_values) #detect optimal number of clusters
+    _, labels, (centers) = cv2.kmeans(pixel_values, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    centers = np.uint8(centers) #convert back to 8 bit values
+
+    ## flatten the labels array
+    labels = labels.flatten()
+    ## convert all pixels to the color of the centroids
+    segmented_image = centers[labels.flatten()]
+    segmented_image = segmented_image.reshape(image.shape)
+    #plt.imshow(segmented_image, interpolation='none',cmap=plt.cm.jet,origin='upper' ) #show the clustered image as a colormap
+    #plt.show()
+    masking(k, labels, pixel_values, image, image_path) #mask undesired portions
+    
+
+if __name__ == "__main__":
+    main()
